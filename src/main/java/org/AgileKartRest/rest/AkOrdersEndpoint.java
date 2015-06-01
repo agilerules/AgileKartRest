2ec385b2f4f0d3d8ffd2fa23571b98fe9d4b48cd
jj <jjeyashree@gmail.com>
2015-06-01 20:06:39 +0530

    Added rest service for order status update

--- src/main/java/org/AgileKartRest/rest/AkOrdersEndpoint.java
+++ src/main/java/org/AgileKartRest/rest/AkOrdersEndpoint.java
@@ -121,4 +121,28 @@ public class AkOrdersEndpoint
 
       return Response.noContent().build();
    }
+   @GET
+   @Path("{id:[0-9][0-9]*}/{status}")
+   @Produces("application/json")
+   public Response updateStatus(@PathParam("id") Integer id,@PathParam("status") String status)
+   {
+      TypedQuery<AkOrders> findOrderQuery = em.createQuery("SELECT DISTINCT a FROM AkOrders a LEFT JOIN FETCH a.akUsers LEFT JOIN FETCH a.akOrderDetailses WHERE a.orderId = :entityId ORDER BY a.orderId", AkOrders.class);
+      findOrderQuery.setParameter("entityId", id);
+      AkOrders entity;
+      try
+      {
+         entity = findOrderQuery.getSingleResult();
+         entity.setOrderStatus(status);
+      }
+      catch (NoResultException nre)
+      {
+         entity = null;
+      }
+      if (entity == null)
+      {
+         return Response.status(Status.NOT_FOUND).build();
+      }
+      return Response.ok(entity).build();
+   }
+
 }
